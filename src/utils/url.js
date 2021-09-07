d288ddbac9f6f0f949cc5c1f4fee7b9f06cf3032
lcwei <lcwei@tw.ibm.com.tw>
2021-09-07 23:48:10 +0800

    [refactor] comment

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,12 +1,14 @@
 export default {
-  // getProductList: (path, rowIndex, pageSize) => `/mock/products/${path}.json?rowIndex=${rowIndex}&pageSize=${pageSize}`,
   getProductList: (path, rowIndex, pageSize) => `http://localhost:8080/jeecg-boot/com.meituan.pd/meituanPd/getProductList?type=${path}&rowIndex=${rowIndex}&pageSize=${pageSize}`,
-  // getProductDetail: (id) => `/mock/product_detail/${id}.json`,
   getProductDetail: (id) => `http://localhost:8080/jeecg-boot/com.meituan.pd/meituanPd/getProductDetail?id=${id}`,
-  // getShopById: (id) => `/mock/shops/${id}.json`,
   getShopById: (id) => `http://localhost:8080/jeecg-boot/com.meituan.shop/meituanShop/getShopById?id=${id}`,
   getPopularKeywords: () => '/mock/keywords/popular.json',
   getRelatedKeywords: (text) => `/mock/keywords/related.json?keyword=${text}`,
   getRelatedShops: (keyword) => `/mock/shops/related.json?keyword=${keyword}`,
   getOrders: () => `/mock/orders/orders.json`, 
+
+  // getShopById: (id) => `/mock/shops/${id}.json`,
+  // getProductDetail: (id) => `/mock/product_detail/${id}.json`,
+  // getProductList: (path, rowIndex, pageSize) => `/mock/products/${path}.json?rowIndex=${rowIndex}&pageSize=${pageSize}`,
+    
 }
